8e7bb7b40becc46963b784b3e7fb9f398057fac3
Carlos E. Contreras Vara <carlosecv@realsystems.com.mx>
2022-06-09 16:45:37 -0500

    [IMP] Elif Otro

---
+++ if_elif_otro.py
@@ -0,0 +1,10 @@
+x = 1
+
+if x > 10:
+    print(" x es mayor que 10!")
+elif x < 20:
+    print("x es menor que 20!")
+elif x < 10:
+    print("x es menor que 10!")
+else:
+    print("x es igual a 10")
