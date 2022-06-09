d6b3f6c7b1c965ff0ea9029a2cba66dbbe46f035
Carlos E. Contreras Vara <carlosecv@realsystems.com.mx>
2022-06-09 16:41:15 -0500

    [IMP] Elif

---
+++ if_elif.py
@@ -0,0 +1,12 @@
+x = 14
+
+if x > 10:
+    print(" x es mayor que 10!")
+elif x < 10:
+    print("x es menor que 10!")
+elif x < 20 :
+    print("x es menor que 20!")
+else:
+    print("x es igual a 10")
+
+print('Fin')
