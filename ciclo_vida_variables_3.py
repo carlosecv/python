9c46bbc63ec0ed5bcd10b00eecfa524fff00a071
Carlos E. Contreras Vara <carlosecv@realsystems.com.mx>
2022-06-21 17:07:40 -0500

    [IMP] ciclo de vida de las variables dentro y fuera de las funciones global

--- ciclo_vida_variables_3.py
+++ ciclo_vida_variables_3.py
@@ -1,10 +1,11 @@
-y = 20
 
 def muestra_x():
     x = 10
+    global y
     y = 9
     print(f'x en la funcion vale {x}')
     print(f'y en la funcion vale {y}')
     
+y = 20
 muestra_x()
 print(f'y fuera de la funcion vale {y}')
