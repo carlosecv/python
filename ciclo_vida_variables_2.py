f66e852f1f91e36b4e0eb7cee4e9fe03a5cfa8a2
Carlos E. Contreras Vara <carlosecv@realsystems.com.mx>
2022-06-21 17:03:37 -0500

    [IMP] ciclo de vida de las variables dentro y fuera de las funciones

---
+++ ciclo_vida_variables_2.py
@@ -0,0 +1,9 @@
+y = 20
+def muestra_x():
+    x = 10
+    y = 9
+    print(f'x en la funcion vale {x}')
+    print(f'y en la funcion vale {y}')
+    
+muestra_x()
+print(f'y fuera de la funcion vale {y}')
