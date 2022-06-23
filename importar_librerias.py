084f0e3c96431792a3433fa8d6e63300c829a450
Carlos E. Contreras Vara <carlosecv@realsystems.com.mx>
2022-06-23 16:57:33 -0500

    [IMP] Importar Librerias de funciones

---
+++ importar_librerias.py
@@ -0,0 +1,5 @@
+#from  mis_funciones import cubo
+
+import mis_funciones
+
+print(mis_funciones.cubo(88))
---
+++ mis_funciones.py
@@ -0,0 +1,9 @@
+def saludo(nombre):
+    print(f'Hola {nombre}')
+
+def cuadrado(x):
+    return x ** 2
+
+def cubo(x):
+    return x ** 3
+
